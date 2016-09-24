58b1a62a6db5c49693bfb7d69e8f48a2670cdfb4
Benjamin Dengler <benjamin.dengler@gmail.com>
2016-09-24 13:13:29 +0200

    Added unity project and basic socket communication

---
+++ unity/Assets/Scripts/SocketInput.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using SocketIO;
+using System.Collections;
+
+public class SocketInput : MonoBehaviour
+{
+    public void Start()
+    {
+        socket = GetComponent<SocketIOComponent>();
+
+        socket.On("connect", OnConnect);
+        socket.On("input", OnInput);
+        socket.On("position", OnPosition);
+    }
+
+    public void OnConnect(SocketIOEvent e)
+    {
+        socket.Emit("register", new JSONObject("\"game\""));
+    }
+
+    public void OnInput(SocketIOEvent e)
+    {
+        Debug.Log(string.Format("Name: {0}, Data: {1}", e.name, e.data));
+    }
+
+    public void OnPosition(SocketIOEvent e)
+    {
+        Debug.Log("Position received");
+    }
+
+    private SocketIOComponent socket;
+}
